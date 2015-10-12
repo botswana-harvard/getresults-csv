f923de3a7d086d171ca82748362980ef51290836
erikvw <ew2789@gmail.com>
2015-10-12 21:35:06 +0200

    update req

--- getresults_csv/settings.py
+++ getresults_csv/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_crypto_fields',
     'getresults_identifier',
     'getresults_patient',
     'getresults_receive',
